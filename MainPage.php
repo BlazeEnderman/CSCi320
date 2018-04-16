6288a195e72dc5d757a085a3fe2a64afb79dc262
Grant Alberts <grant.alberts010@gmail.com>
2018-04-16 13:04:39 -0500

    loginStuff

---
+++ MainPage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PUBG</title>
+  </head>
+  <body>
+    Pubbles Grubbles is a bad game. Thats about it ok thats it folks lets go home.
+  </body>
+</html>
