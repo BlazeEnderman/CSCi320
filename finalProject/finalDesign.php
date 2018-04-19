4054bc9993c5b569e4f3a74966677898c25b1c0a
Brian <brianj868@yahoo.com>
2018-04-19 12:24:28 -0500

    g

--- finalProject/finalDesign.php
+++ finalProject/finalDesign.php
@@ -6,11 +6,21 @@
 
   <link rel="stylesheet" href="stylez.css" />
 </head>
+<header>
+  <img src="PUBG_Header.png" alt="HeaderIMG">
+</header>
 <body>
 <a href="#">Login/Create Account</a>
-<div class="search">
-  <input type="text" placeholder="Search...">
+<div class="navBar">
+  <a href="#">Home</a>
+  <a href="#">Contact</a>
+  <a href="#">About</a>
+  <a href="#">FAQ</a>
 </div>
+<div class="video">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qJfNdN2_rGg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>
+
 
 </body>
 <html>
