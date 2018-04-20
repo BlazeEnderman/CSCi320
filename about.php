7976341fc170c6a813d89d1d03322bc7ee19d119
Brian <brianj868@yahoo.com>
2018-04-20 02:21:51 -0500

    f

--- about.php
+++ about.php
@@ -10,15 +10,9 @@
         <img src="PUBG_Header.png" alt="HeaderIMG">
 
         <p> <ul>
-<<<<<<< HEAD
           <li> <a href="finalProject\finalDesign.php">Home</a> </li>
           <li> <a href="contact.php">Contact</a> </li>
           <li> <a href="about.php">About Us</a> </li>
-=======
-          <li> <a href="#">Home</a> </li>
-          <li> <a href="#">Contact</a> </li>
-          <li> <a href="#">About Us</a> </li>
->>>>>>> f31bef99ab047941809f5d33dc87105829bc5be6
           <li> <a href="#">FAQ</a></li>
         </ul> </p>
       </header>
