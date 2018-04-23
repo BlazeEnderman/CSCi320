dcdd555b2be34d5fa6d6be770018342edd8c5560
Grant Alberts <grant.alberts010@gmail.com>
2018-04-22 22:02:54 -0500

    Comment page, not Functioning

---
+++ comments.php
@@ -0,0 +1,56 @@
+<?php
+
+//GET 10 LATEST comments
+require_once('sqlthings/config.php');
+function outputComments()
+{
+  try {
+      $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "SELECT * FROM comments";
+      $result = $pdo->query($sql);
+      while ($row = $result-fetch()) {
+        outputSingleComment($row);
+      }
+      $pdo=null;
+  } catch (PDOException $e) {
+    die($e->getMessage());
+  }
+}
+
+function outputSingleComment($row)
+{
+  echo "<h2>What " . $row['firstName'] . " " . $row['lastName'] . " says about PUBG: </h2>" .
+      "<p>" . $row['userComment'] . "</p>";
+}
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <main>
+      <header>
+        <img src="PUBG_Header.png" alt="HeaderIMG" class = "headPic">
+        <p> <ul>
+          <li> <a href="finalProject\finalDesign.php">Home</a> </li>
+          <li> <a href="contact.php">Contact</a> </li>
+          <li> <a href="about.php">About Us</a> </li>
+          <li> <a href="faq.php">FAQ</a></li>
+        </ul> </p>
+      </header>
+      <section>
+        <h1>What people are saying about PUBG</h1>
+        <br>
+        <?php outputComments() ?>
+      </section>
+
+    </main>
+  </body>
+</html>
